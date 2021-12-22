8691140ca01041a6d4e2f394420368a129b22afb
Syed Hossen <90913593+syedhossen@users.noreply.github.com>
2021-12-22 18:46:52 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,80 +1,41 @@
-function textChange(){
-  document.getElementById('p').innerHTML="Journey with JS";
-}
+$(document).ready(function(){
 
-$( function() {
-    $( "#draggable" ).draggable();
+//counter up
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+//wow
+wow = new WOW(
+                      {
+                      boxClass:     'wow',      
+                      animateClass: 'animated', 
+                      offset:       0,          
+                      mobile:       true,       
+                      live:         true        
+                    }
+                    )
+  //AOS                  wow.init();
 
-  } );
+ AOS.init();
 
-$( function() {
-    $( "#selectable" ).selectable();
-  } );
-$( function() {
-    $( "#sortable" ).sortable();
-  } );
-//Accordion
-$( function() {
-    $( "#accordion" ).accordion();
-  } );
 
-//AutoComplete
+      //Curved Text
+     
+    var arc = function (t)
+    {
+        return {
+            x: 500.0 + 300.0 * Math.cos(2.0 * Math.PI * t - 0.5 * Math.PI),
+            y: 400.0 + 300.0 * Math.sin(2.0 * Math.PI * t - 0.5 * Math.PI)
+        };
+    };
 
-$( function() {
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
+    $('#text1').curvedText({
+        curve:    arc,
+        domain:   [-0.2, 0.2],
+        viewport: { x: 0.0, y: 0.0, width: 1000.0, height: 400.0 }
     });
-  } );
 
 
-  //checkradiobox
-  $( function() {
-    $( "input" ).checkboxradio();
-  } );
-
-
-  $( function() {
-    $( "#datepicker" ).datepicker();
-  } );
-
-  $( function() {
-    $( "#menu" ).menu();
-  } );
-
-
-  $( function() {
-    $( "#speed" ).selectmenu();
- 
-    $( "#files" ).selectmenu();
- 
-    $( "#number" )
-      .selectmenu()
-      .selectmenu( "menuWidget" )
-        .addClass( "overflow" );
- 
-    $( "#salutation" ).selectmenu();
-  } );
+    
+});
